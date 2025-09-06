f9d1a1bd6a26de57004bdecbab73fb0b3f960689
Aamer Kazi <aamerk4716@gmail.com>
2025-09-06 12:50:34 -0700

    chore: set basePath/assetPrefix for GitHub Pages in CI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1] || '';
+
+const basePath = isCI && repoName ? `/${repoName}` : '';
+
 const nextConfig = {
   output: 'export',
   images: {
@@ -6,6 +11,8 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true, // safer for static hosts like GitHub Pages
+  basePath,
+  assetPrefix: basePath,
 };
 
 module.exports = nextConfig;
